d83d9c3177405746520da64ff902655cc030fd29
Eda <eda.isaku@yahoo.com>
2023-01-08 22:31:41 +0100

    ADD: Bottom Tab Navigator

--- src/screens/index.tsx
+++ src/screens/index.tsx
@@ -1,4 +1,5 @@
 export { IntroScreens } from "./IntroScreens";
 export { Home } from "./Home";
+export { Search } from "./Search";
 export { Ticket } from "./Ticket";
 export { User } from "./User";
