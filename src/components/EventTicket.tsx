1ee4693fe690a6a2044179f31f34fd8f66f65d39
Eda <eda.isaku@yahoo.com>
2023-02-02 10:12:33 +0100

    EDIT: Ticket Screen design

--- src/components/EventTicket.tsx
+++ src/components/EventTicket.tsx
@@ -2,17 +2,8 @@ import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { COLORS, POSITION, SIZES } from "../../theme/theme";
 import QRCode from "react-native-qrcode-svg";
-import {
-  GestureHandlerRootView,
-  Gesture,
-  GestureDetector,
-} from "react-native-gesture-handler";
-import Animated, {
-  useAnimatedStyle,
-  useSharedValue,
-  withSpring,
-} from "react-native-reanimated";
-
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import Animated, { useSharedValue } from "react-native-reanimated";
 export const EventTicket = ({
   name,
   lName,
@@ -23,75 +14,35 @@ export const EventTicket = ({
   ticketNumber: Number;
 }) => {
   const initials = name[0].toUpperCase();
-  const username = `/${name}${lName}`;
-
-  const isPressed = useSharedValue(false);
-  const offset = useSharedValue({ x: 0, y: 0 });
-  const animatedStyles = useAnimatedStyle(() => {
-    return {
-      transform: [
-        { translateX: offset.value.x },
-        { translateY: offset.value.y },
-      ],
-    };
-  });
-  const start = useSharedValue({ x: 0, y: 0 });
-  const gesture = Gesture.Pan()
-    .onBegin(() => {
-      isPressed.value = true;
-    })
-    .onUpdate((e) => {
-      // console.log(e.translationX, e.translationY);
-
-      offset.value = {
-        x: e.translationX + start.value.x,
-        y: e.translationY + start.value.y,
-      };
-    })
-    .onEnd(() => {
-      start.value = {
-        x: 0,
-        y: 0,
-      };
-    })
-    .onFinalize(() => {
-      isPressed.value = false;
-    });
 
   return (
-    <GestureHandlerRootView>
-      <GestureDetector gesture={gesture}>
-        <Animated.View style={[styles.container, animatedStyles]}>
-          <View style={[styles.circle, styles.topCircle]}></View>
-          <View style={styles.upperContainer}>
-            <View style={styles.initialsContainer}>
-              <Text style={styles.initials}>{initials}</Text>
-            </View>
-            <View style={styles.usernameContainer}>
-              <Text style={styles.name}>{`${name} ${lName}`}</Text>
-              <Text style={styles.username}>{username}</Text>
-            </View>
-            <View style={styles.QRContainer}>
-              <QRCode size={110} value="EDAISAKU" />
-            </View>
-            <View style={styles.eventDataContainer}>
-              <View style={styles.eventNameContainer}>
-                <Text style={styles.eventName}>YOUTH</Text>
-                <View style={styles.eventTypeContainer}>
-                  <Text style={styles.eventType}>FESTIVAL</Text>
-                </View>
-              </View>
-              <Text style={styles.eventDate}>10:30 AM Oct 25, 2023.</Text>
+    <View style={[styles.container]}>
+      <View style={[styles.circle, styles.topCircle]}></View>
+      <View style={styles.upperContainer}>
+        <View style={styles.usernameContainer}>
+          <Text style={styles.name}>{`${name} ${lName}`}</Text>
+        </View>
+        <View style={styles.QRContainer}>
+          <View style={styles.initialsContainer}>
+            <Text style={styles.initials}>{initials}</Text>
+          </View>
+          <QRCode size={190} value="EDAISAKU" />
+        </View>
+        <View style={styles.eventDataContainer}>
+          <View style={styles.eventNameContainer}>
+            <Text style={styles.eventName}>YOUTH</Text>
+            <View style={styles.eventTypeContainer}>
+              <Text style={styles.eventType}>FESTIVAL</Text>
             </View>
           </View>
-          <View style={styles.dottedContainer}></View>
-          <View style={styles.lowerContainer}>
-            <Text style={styles.ticketNumber}>{`#${ticketNumber}`}</Text>
-          </View>
-          <View style={[styles.circle, styles.bottomCircle]}></View>
-        </Animated.View>
-      </GestureDetector>
-    </GestureHandlerRootView>
+          <Text style={styles.eventDate}>10:30 AM Oct 25, 2023</Text>
+        </View>
+      </View>
+      <View style={styles.lowerContainer}>
+        <Text style={styles.ticketNumber}>{`#${ticketNumber}`}</Text>
+      </View>
+      <View style={[styles.circle, styles.bottomCircle]}></View>
+    </View>
   );
 };
 
@@ -109,66 +60,74 @@ const styles = StyleSheet.create({
     justifyContent: "space-around",
     alignItems: "center",
     width: "100%",
-    height: "75%",
-  },
-  dottedContainer: {
-    width: SIZES.phoneDimensions.width - 75,
-    // width: "100%",
-    height: 2,
-    borderWidth: 0.7,
-    borderColor: COLORS.transparentWhite,
-    borderStyle: "dashed",
-    // backgroundColor: "red",
+    height: "80%",
   },
   lowerContainer: {
     ...POSITION.center,
     width: "100%",
-    height: "25%",
+    height: "20%",
     paddingBottom: SIZES.padding * 2,
+    borderTopColor: COLORS.transparentWhite,
+    borderTopWidth: 0.5,
   },
   circle: {
     position: "absolute",
-    width: 80,
-    height: 40,
+    width: 60,
+    height: 30,
     borderColor: COLORS.transparentWhite,
     borderWidth: 1,
   },
   topCircle: {
     top: -1,
-    borderBottomLeftRadius: 40,
-    borderBottomRightRadius: 40,
+    borderBottomLeftRadius: 30,
+    borderBottomRightRadius: 30,
     borderTopColor: COLORS.black,
   },
   bottomCircle: {
     bottom: -1,
-    borderTopLeftRadius: 40,
-    borderTopRightRadius: 40,
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
     borderBottomColor: COLORS.black,
   },
   initialsContainer: {
+    position: "absolute",
+    top: -20,
     ...POSITION.center,
-    width: 70,
-    height: 70,
-    borderRadius: 35,
+    width: 40,
+    height: 40,
+    borderRadius: 30,
+    backgroundColor: "#fff",
     borderColor: COLORS.transparentWhite,
     borderWidth: 1,
-    marginTop: 50,
   },
   usernameContainer: {
     ...POSITION.center,
+    marginTop: 25,
   },
   QRContainer: {
-    ...POSITION.center,
-    width: 120,
-    height: 120,
-    backgroundColor: COLORS.white,
+    width: 220,
+    height: 250,
+    borderRadius: 25,
+    backgroundColor: COLORS.black,
+    borderWidth: 1,
+    justifyContent: "flex-end",
+    alignItems: "center",
+    paddingBottom: 25,
+    borderColor: "#020202",
+    shadowColor: "#515151",
+    shadowOffset: {
+      width: 0,
+      height: 0,
+    },
+    shadowOpacity: 0.9,
+    shadowRadius: 10,
   },
   eventDataContainer: {
     justifyContent: "center",
     alignItems: "center",
     flexDirection: "row",
     width: "100%",
-    height: "10%",
+    height: "8%",
   },
   eventNameContainer: {
     flexDirection: "row",
@@ -182,26 +141,21 @@ const styles = StyleSheet.create({
     borderBottomWidth: 2,
   },
   initials: {
-    fontFamily: "Lato-Bold",
-    fontSize: SIZES.largeTitle,
+    fontFamily: "Lato-Black",
+    fontSize: SIZES.h1,
     color: COLORS.secondary,
   },
   name: {
     color: COLORS.white,
     fontFamily: "Lato-Bold",
-    fontSize: SIZES.h1 + 10,
-    letterSpacing: -2,
-  },
-  username: {
-    color: COLORS.white,
-    fontFamily: "Lato-Regular",
-    fontSize: SIZES.body2,
+    fontSize: SIZES.h1,
+    letterSpacing: -1,
   },
   ticketNumber: {
     color: COLORS.white,
-    fontSize: SIZES.largeTitle,
+    fontSize: SIZES.h1,
     fontFamily: "Orbitron-Regular",
-    letterSpacing: 3,
+    letterSpacing: 5,
   },
   eventName: {
     color: COLORS.white,
@@ -211,6 +165,7 @@ const styles = StyleSheet.create({
   eventType: {
     color: COLORS.white,
     padding: 5,
+    marginLeft: 5,
   },
   eventDate: {
     color: COLORS.white,
